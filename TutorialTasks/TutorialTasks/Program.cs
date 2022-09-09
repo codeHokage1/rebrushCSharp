69994c97eda86a8abbf2dcb1cd746a85cd347722
Sodiq Farhan A <sodiqfarhan360@gmail.com>
2022-09-09 12:40:03 +0100

    Add file for all other tasks

---
+++ TutorialTasks/TutorialTasks/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TutorialTasks
+{
+    internal class Program
+    {
+        // Method 1: To take a parameter and perform an action
+        public static void Greet(string name)
+        {
+            Console.WriteLine("Hello there, " + name + "!");
+        }
+
+
+        // Method 2: Product method
+        public static int Product(int x, int y)
+        {
+            return x * y;
+        }
+
+
+
+        // Main
+        static void Main(string[] args)
+        {
+            Greet("Farhan");
+            Console.WriteLine("3 X 5 is : " + Product(3, 5));
+        }
+
+    }
+}
